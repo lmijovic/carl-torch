7b35e4c19c4c9b4a249e01e7ce166e7fbd1b05f2
lmijovic <liza.mijovic@cern.ch>
2021-03-04 00:31:57 +0000

    adding tH variables & clean-up redundant print in ml/base

---
+++ variables_edb/variables_tH_particle.py
@@ -0,0 +1,6 @@
+def get_variable_names(p_dummy=False):
+    
+    # names of variables to use:
+    names_all = ["evnum","is_tdec_lept","y1_pt","y1_eta","y1_phi","y1_e","y2_pt","y2_eta","y2_phi","y2_e","yy_pt","yy_eta","yy_phi","yy_e","nlep","nbjet","njet","lep_pt","lep_eta","lep_phi","lep_e","met_pt","met_eta","met_phi","met_e","W_pt","W_eta","W_phi","W_e","b1_pt","b1_eta","b1_phi","b1_e","b2_pt","b2_eta","b2_phi","b2_e","j1_pt","j1_eta","j1_phi","j1_e","j2_pt","j2_eta","j2_phi","j2_e","j3_pt","j3_eta","j3_phi","j3_e","sum_pt"]
+        
+    return(names_all)
