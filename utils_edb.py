3fe44917e921e6f4e052add66698798179dea0aa
lmijovic <liza.mijovic@cern.ch>
2021-06-05 18:38:30 +0100

    rework utils so that funciton calls can also be done on single file

--- utils_edb.py
+++ utils_edb.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
 
+# carl torch framework has a bunch of requirements on inputs;
+# aim of this module is to ensure these requirements are satisfied 
+
+def preprocess_input_file(df_in):
+    # requirements on single file 
+
+    # if weights exits, normalize weights to 1:
+    if 'weight' in df_in:
+        weight_sum = df_in['weight'].sum()
+        df_in['weight']=df_in['weight']*df_in.shape[0]/weight_sum
+
+    # * no NaN-s
+    df_in=df_in.fillna(-999.)
+
+    return(df_in)
+
+
 def preprocess_inputs(df0_in,df1_in,cleaning_split=0.05,random_seed=42):
-    # framework has a bunch of requirements on inputs;
-    # ensure these are satisfied 
     # df0_in and df1_in = signal and background inputs 
     # cleaning_split : fraction of sample to use to ensure all carl-torch
     # requirements are satisfied.  Should be comparable/smaller
     # to your tain/val split, so that each of train and val
     # is likely to satisfy requirements individually
 
-    # if weights exits, normalize weights to 1 in each sample:
-    if 'weight' in df0_in:
-        weight_sum_new = df0_in['weight'].sum()
-        weight_sum_old = df1_in['weight'].sum()
-        df0_in['weight']=df0_in['weight']*df0_in.shape[0]/weight_sum_new
-        df1_in['weight']=df1_in['weight']*df1_in.shape[0]/weight_sum_old
+    # requirements on individual file: 
+    df0_in = preprocess_input_file(df0_in)
+    df1_in = preprocess_input_file(df1_in)
 
-
-    # * no NaN-s
-    df0_in=df0_in.fillna(-999.)
-    df1_in=df1_in.fillna(-999.)
+    # requirements on combination of both files 
     _df_all = pd.concat([df0_in,df1_in])
     # randomize & get a sub-sample for cleaning:
     _df_all_p1,_df_all_p2  = \
@@ -36,3 +45,6 @@ def preprocess_inputs(df0_in,df1_in,cleaning_split=0.05,random_seed=42):
 
     # filter input samples to keep only these columns:
     return(df0_in[_comp.index],df1_in[_comp.index])
+
+
+
