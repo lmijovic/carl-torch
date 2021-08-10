23fc13d38f488c1589a9cb824155721c15c437b5
lmijovic <liza.mijovic@cern.ch>
2021-08-10 17:32:20 +0100

    adding event weights to test inputs

---
+++ tests/inputs/variables_test.py
@@ -0,0 +1,7 @@
+def get_variable_names():
+    
+    # names of all variables in the input file
+    # (used to load the csv)
+    names_all = ["eventnumber","x","eventweight"]
+
+    return(names_all)
