bd4e483b3c6a3685a874ba9c33d4dd5be524627d
lmijovic <liza.mijovic@cern.ch>
2021-08-15 00:35:20 +0100

    Revert "adding event weights to test inputs"
    This reverts commit 23fc13d38f488c1589a9cb824155721c15c437b5.

--- tests/inputs/generate_inputs.py
+++ tests/inputs/generate_inputs.py
@@ -5,15 +5,12 @@
 import csv
 import numpy as np
 
-def make_sample(mu=0.,sigma=1.,nevents=1000,start_eventnumber=1,weight=1):
+def make_sample(mu=0.,sigma=1.,nevents=1000,start_eventnumber=1):
     s = np.random.normal(mu, sigma, nevents)
     # toy event index:
     enum = np.arange(start_eventnumber, start_eventnumber+nevents, 1, dtype=int)
-    # toy weight:
-    eventweight = weight*np.ones(nevents)
     # concatenate 
     s = np.append(enum.reshape(nevents,1), s.reshape(nevents,1), axis=1)
-    s = np.append(s,eventweight.reshape(nevents,1), axis=1)
     return(s)
 
 def write_to_csv(sample,filename):    
@@ -29,7 +26,7 @@ def write_to_csv(sample,filename):
 # reference sample:
 start_en=1
 nevents=100000
-s = make_sample(mu=0,sigma=1,nevents=nevents,start_eventnumber=start_en,weight=1)
+s = make_sample(mu=0,sigma=1,nevents=nevents,start_eventnumber=start_en)
 # reference file, which will get the weights to reweight it to newfile
 write_to_csv(s,"old.csv")
 
@@ -37,8 +34,8 @@ write_to_csv(s,"old.csv")
 start_en+=(start_en+nevents)*10
 nevents=100000
 # new sample, to which we reweight:
-s1 = make_sample(mu=-0.5,sigma=0.3,nevents=nevents,start_eventnumber=start_en,weight=0.5)
-s2 = make_sample(mu=0.5,sigma=0.3,nevents=nevents,start_eventnumber=start_en+nevents+1,weight=2)
+s1 = make_sample(mu=-0.5,sigma=0.3,nevents=nevents,start_eventnumber=start_en)
+s2 = make_sample(mu=0.5,sigma=0.3,nevents=nevents,start_eventnumber=start_en+nevents+1)
 s=np.concatenate((s1,s2), axis=0)
 # write file with distribution to which we want to reweight
 write_to_csv(s,"new.csv")
--- tests/inputs/variables_test.py
+++
@@ -1,7 +0,0 @@
-def get_variable_names():
-    
-    # names of all variables in the input file
-    # (used to load the csv)
-    names_all = ["eventnumber","x","eventweight"]
-
-    return(names_all)
